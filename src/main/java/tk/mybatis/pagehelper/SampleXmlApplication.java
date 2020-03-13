afea7e31da05030fa6a93fa572e0a530e555cba6
xuqingfeng1212 <xuqingfeng1212@163.com>
2020-03-13 14:53:08 +0800

    16

--- src/main/java/tk/mybatis/pagehelper/SampleXmlApplication.java
+++ src/main/java/tk/mybatis/pagehelper/SampleXmlApplication.java
@@ -46,7 +46,7 @@ public class SampleXmlApplication implements CommandLineRunner {
     }
 
     public void run(String... args) throws Exception {
-        PageHelper.startPage(1, 22); // 15
+        PageHelper.startPage(1, 22); // 16
         List<Country> countries = countryMapper.selectAll();
         System.out.println("Total: " + ((Page) countries).getTotal());
         for (Country country : countries) {
