3a6d06dd02f9ab5ee551157661758be8a948eddc
xuqingfeng1212 <57405197+xuqingfeng1212@users.noreply.github.com>
2020-03-13 10:54:33 +0800

    Update SampleXmlApplication.java

--- src/main/java/tk/mybatis/pagehelper/SampleXmlApplication.java
+++ src/main/java/tk/mybatis/pagehelper/SampleXmlApplication.java
@@ -46,7 +46,7 @@ public class SampleXmlApplication implements CommandLineRunner {
     }
 
     public void run(String... args) throws Exception {
-        PageHelper.startPage(1, 22); // test55
+        PageHelper.startPage(1, 22); // test66
         List<Country> countries = countryMapper.selectAll();
         System.out.println("Total: " + ((Page) countries).getTotal());
         for (Country country : countries) {
